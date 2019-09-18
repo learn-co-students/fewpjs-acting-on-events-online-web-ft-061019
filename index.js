90d30830555fb6df506af167a16b132e85d72d96
Jeremiah Rodden <jrodden1@gmail.com>
2019-09-18 11:26:51 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "green"
+
+document.addEventListener("keydown", function(e) {
+   if (e.key === "ArrowLeft") {
+     moveDodgerLeft();
+   } else if (e.key === "ArrowRight") {
+   moveDodgerRight();
+   }
+ });
+
+function moveDodgerLeft(params) {
+   let leftNumbers = dodger.style.left.replace("px", "");
+   let left = parseInt(leftNumbers, 10);
+   
+   if (left > 0) {
+         dodger.style.left = `${left - 1}px`;
+   }  
+}
+
+function moveDodgerRight(params) {
+   let leftNumbers = dodger.style.left.replace("px", "");
+   let left = parseInt(leftNumbers, 10);
+   
+   if (left < 361) {
+         dodger.style.left = `${left + 1}px`;
+   }    
+}
