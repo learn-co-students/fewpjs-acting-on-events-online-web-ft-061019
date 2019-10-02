6e639c82384f8a4bea41f445560a8bed07abd24f
Enoch2k2 <enoch2k2@hotmail.com>
2019-10-02 18:11:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+
+const getDodger = () => document.querySelector("#dodger");
+
+const moveDodgerLeft = () => {
+  let dodger = getDodger();
+  let left = parsePos(dodger.style.left);
+  if(left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+const moveDodgerRight = () => {
+  let dodger = getDodger();
+  let left = parsePos(dodger.style.left);
+  if(left < (400 - dodger.style.width)) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+const parsePos = string => parseInt(string.split("px")[0], 10);
